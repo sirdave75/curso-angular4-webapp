0d1c62801d4af4bc3c04cd1b1600bb18f6a4ce44
David Romanillos <david.romanillos@sildaviaweb.com>
2017-07-30 19:41:25 +0200

    Crear servicio listado de productos

--- src/app/components/productos-list.component.ts
+++ src/app/components/productos-list.component.ts
@@ -1,9 +1,11 @@
-import  { Component } from '@angular/core';
-import  { Router, ActivatedRoute, Params } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ProductoService } from '../services/producto.service';
 
 @Component({
     selector: 'productos-list',
-    templateUrl: '../views/productos-list.html'
+    templateUrl: '../views/productos-list.html',
+    providers: [ProductoService]
 })
 
 export  class ProductosListComponent{
@@ -11,12 +13,14 @@ export  class ProductosListComponent{
 
     constructor(
         private _route: ActivatedRoute,
-        private _router: Router
+        private _router: Router,
+        private _productoService: ProductoService
     ){
         this.titulo = 'Listado de productos';
     }
 
     ngOnInit(){
         console.log('productos-list.component.ts cargado');
+        alert(this._productoService.getProductos());
     }
 }
---
+++ src/app/models/producto.ts
@@ -0,0 +1,11 @@
+export class Producto{
+
+    constructor(
+        public id: number,
+        public nombre: string,
+        public descripcion: string,
+        public precio: number,
+        public imagen: string
+
+    ){}
+}
