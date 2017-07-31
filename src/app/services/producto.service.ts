48d19012df31d28b84115e932809a4f6dc0ea625
David Romanillos <david.romanillos@sildaviaweb.com>
2017-07-31 12:13:27 +0200

    Subida de ficheros y guardado de nombre en BBDD

--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -21,4 +21,38 @@ export class ProductoService{
 
            return this._http.get(this.url + 'productos').map(res => res.json());
     }
+
+    addProducto(producto: Producto){
+            let json = JSON.stringify(producto);
+            let params = 'json=' + json;
+            let headers = new Headers({'Content-Type': 'application/x-www-form-urlencoded'});
+
+            return this._http.post(this.url + 'productos', params, {headers: headers})
+                .map(res => res.json());
+    }
+
+    /*enviar archivos tipe file al servidor*/
+    makeFileRequest(url: string, params:Array<string>, files: Array<File>){
+        return new Promise((resolve, reject)=>{
+            var formData: any = new FormData();
+            var xhr = new XMLHttpRequest();
+
+            for(var i = 0; i<files.length;i++){
+                formData.append('uploads[]',files[i],files[i].name);
+            }
+
+            xhr.onreadystatechange = function(){
+                if(xhr.readyState == 4){
+                    if(xhr.status == 200){
+                        resolve(JSON.parse(xhr.response));
+                    }
+                    else{
+                        reject(xhr.response);
+                    }
+                }
+            }
+            xhr.open("POST", url, true);
+            xhr.send(formData);
+        });
+    }
 }
